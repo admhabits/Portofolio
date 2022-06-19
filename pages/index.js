5a9982f00c20a5599592294ff2505b7620e905dd
Alam Wibowo <alamhafidz61@gmail.com>
2022-06-20 04:53:56 +0700

    Update

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,16 @@ export default function Home() {
             <h2 className="heading">Alam Santiko Wibowo</h2>
             <div className="line"></div>
             <p className="text-content">
+             {
+              `
               Hallo. Let me introduce, I'm Alam Santiko Wibowo,
               Full Stack React Developer Web &amp; Mobile Apps from Cilacap, Central Java - Indonesia.
               I really like to learn something new and interact with a lots of people.
               If you have any idea project to work with together.
               I will help you, to do something of your goal.
               Based on requirement as Programmer, I'm good at Git System Versioning &amp; RDBMS SQL and No-SQL. I can working with team collaboration.
+              `
+             }
             </p>
           </div>
           <div className="side-content">
