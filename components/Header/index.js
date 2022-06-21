52b64adb18b04466f738473e165909725f294012
alam.santiko <alamhafidz61@gmail.com>
2022-06-21 09:43:08 +0700

    update icon

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,28 +4,43 @@ export default function Header() {
             <ul className="dekstop">
                 <div className="navlink-left">
                     <li>
-                        <a href="#store" className="navlink">
+                        <a href="/store" className="navlink">
                             <i className="material-icons md-30">store</i>
                             <span>Toko</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#hire" className="navlink">
+                        <a href="/offers" className="navlink">
                             <i className="material-icons-outlined md-30">work_history</i>
                             <span>Penawaran</span>
                         </a>
                     </li>
                 </div>
                 <div className="navlink-middle">
-                    <li className="navlink">Pengalaman</li>
-                    <li className="navlink">Portofolio</li>
-                    <li className="navlink">Pendidikan</li>
+                    <li>
+                        <a href="/profile" className="navlink">
+                            <i className="material-icons-outlined md-30">apartment</i>
+                            <span>Profile</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/experience" className="navlink">
+                            <i className="material-icons-outlined md-30">history</i>
+                            <span>Experience</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/education" className="navlink">
+                            <i className="material-icons-outlined md-30">school</i>
+                            <span>Education</span>
+                        </a>
+                    </li>
                 </div>
                 <div className="navlink-right">
                     <li>
-                        <a href="#masuk" className="navlink signBtn">
+                        <a href="/login" className="navlink signBtn">
                             {/* <i className="material-icons md-30">account_circle</i> */}
-                            <span>Masuk</span>
+                            <span>Login</span>
                         </a>
                     </li>
                 </div>
