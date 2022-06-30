193e44f07a57d3698c7276295f05c9f9d4a9f604
alam.santiko <alamhafidz61@gmail.com>
2022-06-30 11:31:42 +0700

    Add All Initial Pages

---
+++ pages/offers/index.js
@@ -0,0 +1,33 @@
+
+import Layout from 'AppComponent/Layouts'
+import Image from 'next/image'
+import ProfilePic from 'public/images/me-trans.png'
+
+export default function Offers() {
+  return (
+    <Layout title="Alam Wibowo | Software Engineer" rows={1}>
+      <div className="main-content">
+        <div className="content">
+          <h2 className="heading">Alam Santiko Wibowo</h2>
+          <div className="line"></div>
+          <p className="text-content">
+            {
+              `
+                Hallo. Let me introduce myself, I'm Alam Santiko Wibowo, Full Stack React Developer Web & Mobile Apps from Cilacap, Central Java - Indonesia. I really like to learn something new and interact with lots of people. Suppose you have any idea project to work with. I will help you to do something about your goal. Based on requirements as a Programmer, I'm good at Git System Versioning & RDBMS SQL and No-SQL. I can work with team collaboration.
+              `
+            }
+          </p>
+          <div className="download-cv">
+            <a href="/CV/CV%20(14).pdf" className="btn btn-primary btn-download">
+              <span className="icon material-icons">file_download</span>
+              <span> Download CV</span>
+            </a>
+          </div>
+        </div>
+        <div className="side-content">
+          <Image src={ProfilePic} width={250} height={250} className="rounded" layout="fixed" objectFit='contain' />
+        </div>
+      </div>
+    </Layout>
+  )
+}
